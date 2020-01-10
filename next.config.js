20a75ad309ffc0bee658667dcd2e5e2835cd2f4e
Israel Wilson <israelsjwilson@gmail.com>
2020-01-09 21:58:40 -0600

    Updating package.json

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ module.exports = {
     pass: 'oyasuminasai',
     client: '997583206714-4dfv16sdss7jnuh6lsnodc151aofhnle.apps.googleusercontent.com',
     secret: 'GgY0GHk1UrIjk_CnERJmZfyD',
-    token: '1/5sE6u1bbKYnUK9bWc9dOss-znBJH6U5vBwxIo-s6FII'
+    token: '1/5sE6u1bbKYnUK9bWc9dOss-znBJH6U5vBwxIo-s6FII',
+    dbHost: 'localhost',
+    dbUser: 'mawlock_mawlock',
+    dbPass: 'nasthalia1',
+    dbName: 'mawlock_tcamechurch'
   },
   exportPathMap: function() {
     return {
