5cbac2269a81c7106167fb8c72ff884f90f12f2b
Israel Wilson <israelsjwilson@gmail.com>
2020-06-04 17:46:36 -0500

    Updating FlexRow.js

--- components/FlexRow.js
+++ components/FlexRow.js
@@ -1,53 +1,61 @@
-export default class FlexRow extends React.Component {
-  constructor(props) {
-      super(props);
-      this.rowRef = React.createRef();
-  }
+export default function FlexRow({display = "flex",
+                                flexDirection = "row",
+                                flexBasisMb = "100%",
+                                flexBasisTl = "47%",
+                                flexBasisDt = "22%",
+                                flexGrow = 1,
+                                flexShrink = 0,
+                                justifyContent = "flex-start",
+                                alignItems = "center",
+                                margin = "1rem",
+                                ...props}) {
+  return (
+    <div>
+      {props.children}
+      <style jsx>{`
 
-  getContainer() {
-    return this.rowRef;
-  };
+        /* Desktop styles */
+        div {
+          display: ${display};
+          flex-direction: ${flexDirection};
+          flex-basis: ${flexBasisDt};
+          flex-grow: ${flexGrow};
+          flex-shrink: ${flexShrink};
+          justify-content: ${justifyContent};
+          align-items: ${alignItems};
+          margin: ${margin};
+        }
 
-  render() {
-    return (
-      <div ref={this.rowRef} name={this.props.name} className={this.props.class}>
-        {this.props.children}
-        <style jsx>{`
+        /* Tablet styles */
+        @media (max-width: 900px) {
           div {
-            display: ${this.props.display};
-            flex-direction: ${this.props.flexDirection};
-            flex-wrap: ${this.props.flexWrap};
-            flex-grow: ${this.props.flexGrow};
-            background: ${this.props.background};
-            background-size: ${this.props.backgroundSize};
-            background-color: ${this.props.backgroundColor};
-            background-blend-mode: ${this.props.backgroundBlendMode};
-            padding-top: ${this.props.paddingTop};
-            padding-bottom: ${this.props.paddingBottom};
-            margin: ${this.props.margin};
-            height: ${this.props.height};
-            justify-content: ${this.props.justify};
-            align-items: ${this.props.align};
+            display: ${display};
+            flex-direction: ${flexDirection};
+            flex-basis: ${flexBasisTl};
+            flex-grow: ${flexGrow};
+            flex-shrink: ${flexShrink};
+            justify-content: ${justifyContent};
+            align-items: ${alignItems};
+            margin: ${margin};
           }
-        `}</style>
-      </div>
-    );
-  }
+        }
 
-  static defaultProps = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    flexGrow: "1",
-    background: "initial",
-    backgroundSize: "cover",
-    backgroundColor: "initial",
-    backgroundBlendMode: "initial",
-    paddingTop: "3rem",
-    paddingBottom: "3rem",
-    margin: "1rem",
-    height: "auto",
-    justifyContent: "flex-start",
-    alignItems: "flex-start"
-  }
+        /* Mobile styles */
+        @media (max-width: 600px) {
+          div {
+            display: ${display};
+            flex-direction: ${flexDirection};
+            flex-basis: ${flexBasisMb};
+            flex-grow: ${flexGrow};
+            flex-shrink: ${flexShrink};
+            justify-content: ${justifyContent};
+            align-items: ${alignItems};
+            margin: ${margin};
+          }
+        }
+
+      `}</style>
+    </div>
+  )
+
 }
