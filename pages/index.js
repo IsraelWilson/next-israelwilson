0ce99869e9408eddca648ea45973a49b72de2d80
Israel Wilson <israelsjwilson@gmail.com>
2020-06-04 17:55:53 -0500

    Adding Home import

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import FlexContainer from '../components/FlexContainer.js'
 import FlexColumn from '../components/FlexColumn.js'
 import FlexRow from '../components/FlexRow.js'
 import JobCard from '../components/JobCard.js'
+import Home from '../components/Home.js'
 
 export default class Index extends React.Component {
   constructor(props) {
