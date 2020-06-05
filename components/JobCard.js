e1074feb288729a1cacf9628d00498bfcf890378
Israel Wilson <israelsjwilson@gmail.com>
2020-06-04 23:03:40 -0500

    Sizing the JobCard

--- components/JobCard.js
+++ components/JobCard.js
@@ -2,7 +2,7 @@ import FlexColumn from '../components/FlexColumn.js'
 
 export default function JobCard(props) {
   return (
-    <FlexColumn>
+    <FlexColumn flexBasisDt="512px" flexBasisMb="512px" flexBasisTl="512px" flexGrow="0">
       <h1><span>{props.company}</span><span>{props.date}</span></h1>
       <p>{props.desc}</p>
       <p className="right">{props.stack}</p>
