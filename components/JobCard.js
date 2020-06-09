c6c17560931da13d48d3999765c5ddfbd19b445f
Israel Wilson <israelsjwilson@gmail.com>
2020-06-08 23:58:40 -0500

    Adding box-shadow to FlexColumn

--- components/JobCard.js
+++ components/JobCard.js
@@ -2,7 +2,7 @@ import FlexColumn from '../components/FlexColumn.js'
 
 export default function JobCard(props) {
   return (
-    <FlexColumn width="512px" bgColor="hsla(199, 98%, 48%, .12)", shadow="0 10px 20px hsla(0, 0%, .15), 0 3px 6px hsla(0, 0%, .10)">
+    <FlexColumn width="512px" bgColor="hsla(199, 98%, 48%, .12)" shadow="0 10px 20px hsla(0, 0%, .15), 0 3px 6px hsla(0, 0%, .10)">
       <h1><span>{props.company}</span><span>{props.date}</span></h1>
       <p>{props.desc}</p>
       <p className="right fade">{props.stack}</p>
